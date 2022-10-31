cc8ce4cbf237553189efe9bb5735d5a73de3714f
GabrielReguera <gabrielreguera121@hotmail.com>
2022-10-31 15:15:46 -0300

    Login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
-import { NavComponent } from "./components/nav/nav.component";
+import { LoginComponent } from "./components/login/login.component";
 import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: NavComponent,
+    component: LoginComponent,
     children: [
       { path: "home", component: HomeComponent },
-      {path: 'tecnicos', component: TecnicoListComponent}
+      { path: "tecnicos", component: TecnicoListComponent },
     ],
+  },
+  {
+    path: "login",
+    component: LoginComponent,
   },
 ];
 
