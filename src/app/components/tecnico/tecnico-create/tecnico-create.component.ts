e79a3b7825527b00e393ef93b3a0a61f306e95c5
GabrielReguera <gabrielreguera121@hotmail.com>
2022-11-03 14:24:10 -0300

    Finish tecnico create

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { Tecnico } from "src/app/models/tecnico";
+import { TecnicoService } from "src/app/services/tecnico.service";
 
 @Component({
   selector: "app-tecnico-create",
@@ -7,16 +11,58 @@ import { FormControl, Validators } from "@angular/forms";
   styleUrls: ["./tecnico-create.component.css"],
 })
 export class TecnicoCreateComponent implements OnInit {
+  tecnico: Tecnico = {
+    id: "",
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    perfis: [],
+    dataCriacao: "",
+  };
+
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.minLength(11));
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() {}
+  constructor(
+    private tecnicoService: TecnicoService,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
-  validaCampos(): boolean{
-    return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid;
+  create(): void {
+    this.tecnicoService.create(this.tecnico).subscribe({
+      next: () => {
+        this.toast.success("Técninco cadastrado com sucesso", "Cadastro");
+        this.router.navigate(["tecnicos"]);
+      },
+      error: (e) => {
+        if (e.error.errors) {
+          e.error.errors.forEach((element) => {
+            this.toast.error(element.message, "Error");
+          });
+        } else {
+          this.toast.error(e.error.message);
+        }
+      },
+    });
+  }
+
+  addPerfil(perfil: any): void {
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
+  }
+
+  validaCampos(): boolean {
+    return (
+      this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+    );
   }
 }
