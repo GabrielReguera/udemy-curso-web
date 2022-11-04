2f35582822f5be5ce020fdfefa92149c99caaffe
GabrielReguera <gabrielreguera121@hotmail.com>
2022-11-04 14:56:25 -0300

    Chamado create form

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-chamado-create',
-  templateUrl: './chamado-create.component.html',
-  styleUrls: ['./chamado-create.component.css']
+  selector: "app-chamado-create",
+  templateUrl: "./chamado-create.component.html",
+  styleUrls: ["./chamado-create.component.css"],
 })
 export class ChamadoCreateComponent implements OnInit {
+  titulo: FormControl = new FormControl(null, Validators.required);
+  status: FormControl = new FormControl(null, Validators.required);
+  prioridade: FormControl = new FormControl(null, Validators.required);
+  descricao: FormControl = new FormControl(null, Validators.required);
+  cliente: FormControl = new FormControl(null, Validators.required);
+  tecnico: FormControl = new FormControl(null, Validators.required);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  validaCampos(): boolean {
+    return (
+      this.titulo.valid &&
+      this.status.valid &&
+      this.prioridade.valid &&
+      this.descricao.valid &&
+      this.cliente.valid &&
+      this.tecnico.valid
+    );
   }
-
 }
