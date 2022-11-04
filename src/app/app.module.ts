20616f9e7fe43df2429ee63e7d173377d15dec11
GabrielReguera <gabrielreguera121@hotmail.com>
2022-11-04 14:44:56 -0300

    Chamado create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-
 import { TecnicoUpdateComponent } from "./components/tecnico/tecnico-update/tecnico-update.component";
 import { AuthInterceptorProvider } from "./interceptors/auth.interceptor";
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 @NgModule({
   declarations: [
@@ -56,6 +57,7 @@ import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-
     ClienteListComponent,
     ClienteUpdateComponent,
     ChamadoListComponent,
+    ChamadoCreateComponent,
   ],
   imports: [
     BrowserModule,
